b691b072a548f3d208242cc119593834c14f4d61
ljxun <591692130@qq.com>
2025-08-07 16:06:08 +0800

    refactor(server_controller): 优化OAS 启动流程
    - 添加任务终止步骤，确保在启动新进程前终止现有 OAS 进程
    - 增加启动和完成提示信息，提高日志可读性
    - 补充终止 pythonw.exe 进程，防止隐藏窗口进程继续运行

--- lib/controller/server/server_controller.dart
+++ lib/controller/server/server_controller.dart
@@ -112,12 +112,17 @@ class ServerController extends GetxController {
     runShell('echo OAS working directory: ').then((value) => null);
     runShell('pwd').then((value) => null);
     // runShell('(type env:path) -split ; ').then((value) => null);
-    runShell('.\\toolkit\\python.exe -m deploy.installer').then((value) => null);
-    runShell('echo Start OAS').then((value) => null);
+    runShell('echo ====================  Taskkill OAS ==================== ').then((value) => null);
+    runShell('taskkill /f /t /im oas.exe').then((value) => null);
     runShell('taskkill /f /t /im python.exe').then((value) => null);
+    runShell('taskkill /f /t /im pythonw.exe').then((value) => null);
+    runShell('echo ====================  Taskkill OAS Complete ==================== ').then((value) => null);
+    runShell('.\\toolkit\\python.exe -m deploy.installer').then((value) => null);
+    runShell('echo ====================  Start OAS ==================== ').then((value) => null);
     // runShell(".\\toolkit\\python.exe  server.py").then((value) => null);
     // runShell(".\\toolkit\\pythonw.exe  server.py").then((value) => null);
     runShell('powershell.exe -Command "Start-Process .\\toolkit\\python.exe -ArgumentList \'server.py\' -WindowStyle Hidden"').then((value) => null);
+    runShell('echo ====================  Start OAS Complete ==================== ').then((value) => null);
   }
 
   void readDeploy() {
